47345206a3cb7528d8ec594b13ceef0cf7bfa4d5
Tri27 <Tri27@gmail.com>
2024-06-25 09:36:22 +0700

    Update

--- CommandLine.h
+++ CommandLine.h
@@ -4,4 +4,3 @@ bool isCommandLine2(int argc, char* argv[]);
 bool isCommandLine3(int argc, char* argv[]);
 bool isCommandLine4(int argc, char* argv[]);
 bool isCommandLine5(int argc, char* argv[]);
-void GenerateData(int a[], int n, int dataType);
