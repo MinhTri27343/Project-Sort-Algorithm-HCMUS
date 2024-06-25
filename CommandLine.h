92973692e84d50a6a2265e7787d358953a05bd31
TriNguyen1208 <168810923+TriNguyen1208@users.noreply.github.com>
2024-06-25 16:51:05 +0700

    Update CommandLine.h của ĐTri

--- CommandLine.h
+++ CommandLine.h
@@ -7,6 +7,8 @@ bool isCommandLine4(int argc, char* argv[]);
 bool isCommandLine5(int argc, char* argv[]);
 =======
 bool isCommandLine5(int argc, char* argv[]);
+void getIndexAlgorithmMode(int argc, char* argv[], int& idxAlgorithmName, int& idxInputOrder, int& idxOutputParameter);
+void getIndexComparisonMode(int argc, char* argv[], int& idxAlgorithmName1, int& idxAlgorithmName2, int& idxInputOrder);
 void GenerateData(int a[], int n, int dataType);
 void runCommandLine(int argc, char* argv[]);
 >>>>>>> Tri27
